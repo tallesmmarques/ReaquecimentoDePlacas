24cfebf03ed663f0da453ad630d4a6aa1c1d3ba5
Talles Marques <tallesemanuel1@gmail.com>
2023-06-10 20:59:22 -0300

    POO na lista circular

--- ProcessoPrincipal/ListaCircular.cpp
+++ ProcessoPrincipal/ListaCircular.cpp
@@ -1,1 +1,60 @@
 #include "ListaCircular.h"
+
+
+ListaCircular::ListaCircular()
+{
+	memoria_processo = new std::string[MAX_DADOS];
+	memoria_otimizacao = new std::string[MAX_DADOS];
+}
+
+ListaCircular::~ListaCircular()
+{
+	delete[] memoria_processo;
+	delete[] memoria_otimizacao;
+}
+
+int ListaCircular::guardarDadoProcesso(char* msg)
+{
+	if (numDadosProcesso + numDadosOtimizacao == MAX_DADOS)
+		return MEMORY_FULL;
+
+	memoria_processo[ultimo_processo] = std::string(msg);
+	ultimo_processo = (ultimo_processo + 1) % MAX_DADOS;
+
+	numDadosProcesso++;
+	return 0;
+}
+int ListaCircular::lerDadoProcesso(char* msg)
+{
+	if (numDadosProcesso == 0)
+		return MEMORY_EMPTY;
+
+	strcpy_s(msg, MAX_MSG, memoria_processo[primeiro_processo].c_str());
+	primeiro_processo = (primeiro_processo + 1) % MAX_DADOS;
+
+	numDadosProcesso--;
+	return 0;
+}
+
+int ListaCircular::guardarDadoOtimizacao(char* msg)
+{
+	if (numDadosOtimizacao + numDadosProcesso == MAX_DADOS)
+		return MEMORY_FULL;
+
+	memoria_otimizacao[ultimo_otimizacao] = std::string(msg);
+	ultimo_otimizacao = (ultimo_otimizacao + 1) % MAX_DADOS;
+
+	numDadosOtimizacao++;
+	return 0;
+}
+int ListaCircular::lerDadoOtimizacao(char* msg)
+{
+	if (numDadosOtimizacao == 0)
+		return MEMORY_EMPTY;
+
+	strcpy_s(msg, MAX_MSG, memoria_otimizacao[primeiro_otimizacao].c_str());
+	primeiro_otimizacao = (primeiro_otimizacao + 1) % MAX_DADOS;
+
+	numDadosOtimizacao--;
+	return 0;
+}
--- ProcessoPrincipal/ListaCircular.h
+++ ProcessoPrincipal/ListaCircular.h
@@ -13,7 +13,7 @@
 
 class ListaCircular
 {
-public:
+private:
 	std::string* memoria_processo;
 	std::string* memoria_otimizacao;
 
@@ -26,61 +26,12 @@ public:
 	int ultimo_otimizacao = 0;
 
 public:
-	ListaCircular()
-	{
-		memoria_processo = new std::string[MAX_DADOS];
-		memoria_otimizacao = new std::string[MAX_DADOS];
-	}
-	~ListaCircular()
-	{
-		delete[] memoria_processo;
-		delete[] memoria_otimizacao;
-	}
+	ListaCircular();
+	~ListaCircular();
 
-	int guardarDadoProcesso(char* msg)
-	{
-		if (numDadosProcesso + numDadosOtimizacao == MAX_DADOS)
-			return MEMORY_FULL;
-
-		memoria_processo[ultimo_processo] = std::string(msg);
-		ultimo_processo = (ultimo_processo + 1) % MAX_DADOS;
-
-		numDadosProcesso++;
-		return 0;
-	}
-	int lerDadoProcesso(char* msg)
-	{
-		if (numDadosProcesso == 0)
-			return MEMORY_EMPTY;
-
-		strcpy_s(msg, MAX_MSG, memoria_processo[primeiro_processo].c_str());
-		primeiro_processo = (primeiro_processo + 1) % MAX_DADOS;
-
-		numDadosProcesso--;
-		return 0;
-	}
-
-	int guardarDadoOtimizacao(char* msg)
-	{
-		if (numDadosOtimizacao + numDadosProcesso == MAX_DADOS)
-			return MEMORY_FULL;
-
-		memoria_otimizacao[ultimo_otimizacao] = std::string(msg);
-		ultimo_otimizacao = (ultimo_otimizacao + 1) % MAX_DADOS;
-
-		numDadosOtimizacao++;
-		return 0;
-	}
-	int lerDadoOtimizacao(char* msg)
-	{
-		if (numDadosOtimizacao == 0)
-			return MEMORY_EMPTY;
-
-		strcpy_s(msg, MAX_MSG, memoria_otimizacao[primeiro_otimizacao].c_str());
-		primeiro_otimizacao = (primeiro_otimizacao + 1) % MAX_DADOS;
-
-		numDadosOtimizacao--;
-		return 0;
-	}
+	int guardarDadoProcesso(char* msg);
+	int lerDadoProcesso(char* msg);
+	int guardarDadoOtimizacao(char* msg);
+	int lerDadoOtimizacao(char* msg);
 };
 
